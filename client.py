5a6fbb3f832f8cceeee3cf771a0e483379d72bc3
zy964c <zy964c@gmail.com>
2017-11-04 23:21:17 +0300

    changed client, passed all tests

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-from socket import *
+import socket
 import time
 
 class ClientError(Exception):
@@ -15,31 +15,32 @@ class Client(object):
         self.host = host
         self.port = port
         self.timeout = timeout
-        self.s = socket(AF_INET, SOCK_STREAM)
-        self.s.settimeout(timeout)
-        #self.s.connect((host, port))
-        #print ("connected")
 
     @staticmethod
-    def parse_metric(metric):
+    def parse_metric(metric_recieved):
         """
         parses data recieved
         """
         dict1 = {}
-        list_metric = metric.split('\\')
+        metric = metric_recieved.decode("utf8")
+        if metric == "error\nwrong command\n\n":
+            raise ClientError
+        #print(metric)
+        list_metric = metric.split('\n')
+        #print(list_metric)
         if len(list_metric) < 4:
             return dict1
         data = list_metric[1:-2]
-        #print data
+        #print(data)
         for i in data:
             list_data = i.split()
             metric_name = list_data[0]
             metric_val = float(list_data[1])
             timestamp = int(list_data[2])
             if metric_name in dict1:
-                dict1[metric_name].append(tuple(timestamp, metric_val))
+                dict1[metric_name].append(tuple([timestamp, metric_val]))
             else:
-                dict1[metric_name] = [tuple(timestamp, metric_val)]
+                dict1[metric_name] = [tuple([timestamp, metric_val])]
         for j in dict1:
             dict1[j].sort(key=lambda tup: tup[0])
         return dict1
@@ -48,31 +49,28 @@ class Client(object):
         """
         Put data on the server
         """
-        self.s.connect((self.host, self.port))
-        try:
-            bytes_sent = self.s.send(bytearray('put {0} {1} {2}\n'.format(metric, m_value, timestamp)))
-            data = self.s.recv(10000)
-        except:
-            self.s.close()
-            raise ClientError
-        if data != 'ok\n\n':
-            self.s.close()
-            raise ClientError
-        #print 'sent {0} bytes of data'.format(bytes_sent)
-        #print data
-        self.s.close()
+        with socket.create_connection((self.host, self.port), self.timeout) as sock:
+            try:
+                #sock.settimeout(self.timeout)
+                data_to_sent = 'put {0} {1} {2}\n'.format(metric, m_value, timestamp)
+                bytes_sent = sock.sendall(data_to_sent.encode(encoding='utf_8'))
+                data = sock.recv(10000)
+            except:
+                raise ClientError
+            #print 'sent {0} bytes of data'.format(bytes_sent)
+            #print data
 
     def get(self, metric):
         """
         gets data from the server
         """
-        self.s.connect((self.host, self.port))
-        try:
-            self.s.send(bytearray('get {0}\n'.format(metric)))
-            data = self.s.recv(10000)
-        except:
-            self.s.close()
-            raise ClientError
-        res = Client.parse_metric(data)
-        self.s.close()
-        return res
+        with socket.create_connection((self.host, self.port), self.timeout) as sock:
+            try:
+                #sock.settimeout(self.timeout)
+                data_to_sent = 'get {0}\n'.format(metric)
+                sock.sendall(data_to_sent.encode(encoding='utf_8'))
+                data = sock.recv(10000)
+            except:
+                raise ClientError
+            res = Client.parse_metric(data)
+            return res
