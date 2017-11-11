7d48441582b701273a809ff7877477c1d723e29c
zy964c <zy964c@gmail.com>
2017-11-12 01:10:09 +0300

    server added

--- server.py
+++ server.py
@@ -25,14 +25,56 @@ class ClientServerProtocol(asyncio.Protocol):
     """
     ClientServerProtocol
     """
+    storage = {}
+
     def connection_made(self, transport):
         self.transport = transport
-        #print(self.transport)
 
     def data_received(self, data):
-        resp = self.process_data(data.decode())
-        self.transport.write(resp.encode())
+        resp = self.process_data(data.decode(encoding='utf_8'))
+        self.transport.write(resp.encode(encoding='utf_8'))
 
-    def process_data(self, data):
-        print(data)
-        return data
+    @classmethod
+    def process_data(cls, data):
+        """
+        processes data
+        """
+        res = []
+        data1 = data.rstrip('\n')
+        data_list = data1.split(' ')
+        if data_list[0] == 'put':
+            if len(data_list) != 4:
+                return 'error\nwrong command\n\n'
+            try:
+                if data_list[1] in cls.storage:
+                    cls.storage[data_list[1]].append((data_list[2], data_list[3]))
+                else:
+                    cls.storage[data_list[1]] = [(data_list[2], data_list[3])]
+            except KeyError:
+                return 'error\nwrong command\n\n'
+
+        elif data_list[0] == 'get':
+            if len(data_list) != 2:
+                return 'error\nwrong command\n\n'
+            if data_list[1] == '*':
+                for key in cls.storage:
+                    for i in cls.storage[key]:
+                        res_str = ''
+                        res_str += key
+                        for j in i:
+                            res_str += (' ' + j)
+                        res.append(res_str)
+            else:
+                if data_list[1] in cls.storage:
+                    for i in cls.storage[data_list[1]]:
+                        res_str = ''
+                        res_str += data_list[1]
+                        for j in i:
+                            res_str += (' ' + j)
+                        res.append(res_str)
+        else:
+            return 'error\nwrong command\n\n'
+
+        res.insert(0, 'ok')
+        res.append('\n')
+        return '\n'.join(res)
